a62ca25ad5ff4baeadf06f6dd45848f477bb7126
Stephen Richer <sr467@bath.ac.uk>
2022-10-01 08:49:22 +0100

    Add pyarrow dependency.

--- setup.py
+++ setup.py
@@ -37,9 +37,10 @@ setup(
         'numpy',
         'plotly',
         'pandas',
+        'pyarrow'
         'seaborn',
         'geopandas',
-        'matplotlib',
+        'matplotlib'
     ],
     license='MIT',
     classifiers=[
