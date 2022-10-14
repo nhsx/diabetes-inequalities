aec6737c9d7a5db6ce0eca4efbb998258ee98038
Stephen Richer <sr467@bath.ac.uk>
2022-10-14 19:19:55 +0200

    Add pformat function.

--- src/esneft_tools/utils.py
+++ src/esneft_tools/utils.py
@@ -15,3 +15,22 @@ def setVerbosity(
     esneft_logger = logging.getLogger('esneft_tools')
     esneft_logger.setLevel(level)
     esneft_logger.addHandler(handler)
+
+
+def formatP(p):
+    """ Return formatted p for title """
+    pformat = 'p '
+    if p > 0.999:
+        pformat += '> 0.999'
+    elif p < 0.001:
+        pformat += '< .001'
+    else:
+        pformat += '= ' + f'{p:.3f}'[1:]
+    if p < 0.001:
+        return pformat + ' ***'
+    elif p < 0.01:
+        return pformat + ' **'
+    elif p < 0.05:
+        return pformat + ' *'
+    else:
+        return pformat
