d6a984ec01bba2fea0a0cbe9110c12051ece819e
Stephen Richer <sr467@bath.ac.uk>
2022-12-22 10:48:30 +0000

    Update restrictions.

--- development/archive/0.getOSMmap/filterOSM.sh
+++
@@ -1,9 +0,0 @@
-# Download from: https://download.geofabrik.de/europe/great-britain/england.html
-
-conda activate ox
-# Extract region by bbox
-osmium extract --bbox 0.477,51.77,1.628,52.61 england-latest.osm.pbf -o esneft.osm
-# Filter for roads only
-osmium tags-filter -o esngeft-highways.osm esneft.osm nw/highway
-
-
